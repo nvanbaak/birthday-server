2825b3ac6ebd5137f00bd6e85aba1b932e850c92
Nikolai <nvanbaak@gmail.com>
2022-06-10 14:58:19 -0700

    added templates from demo project

--- flask-project/app.py
+++ flask-project/app.py
@@ -14,3 +14,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 login.init_app(app)
+
+#####################################
+#              Setup
+#####################################
+
+def addUser(email, password):
+    """
+    Adds a user to the user database if not already present.
+    Returns True if operation was successful, False otherwise.
+    """
+    # check if email exists
+    user = UserModel.query.filter_by(email).first()
+    if user is None:
+
+        # create db entry and submit
+        user = UserModel()
+        user.set_password(password)
+        user.email=email
+        db.session.add(user)
+        db.session.commit()
+        return True
+    else: return False
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+    user = UserModel.query.filter_by(email = "lhhung@uw.edu").first()
+    if user is None:
+        addUser("lhhung@uw.edu","qwerty")
+
--- flask-project/forms/login_form.py
+++ flask-project/forms/login_form.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class loginForm(FlaskForm):
     email=StringField(label="Enter email", validators=[DataRequired(),Email()])
-    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=16)])
+    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=20)])
     submit=SubmitField(label="Login")
